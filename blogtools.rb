6eac87f69531abddd3947558533b304ad5b9c188
dchae <daniel.j.chae@gmail.com>
2024-03-17 16:07:39 -0700

    Updated Post class to accept last_updated_date column values

--- blogtools.rb
+++ blogtools.rb
@@ -1,14 +1,15 @@
 class Post
-  def initialize(id, title, user_id, username, timestamp, content)
+  def initialize(id, title, user_id, username, timestamp, updated_on, content)
     @id = id
     @title = title
     @user_id = user_id
     @username = username
     @timestamp = timestamp
+    @updated_on = updated_on
     @content = content
   end
 
-  attr_reader :id, :title, :user_id, :username, :timestamp, :content
+  attr_reader :id, :title, :user_id, :username, :timestamp, :updated_on, :content
 
   def self.list_from_PG(result)
     result.map do |tuple|
@@ -17,8 +18,9 @@ class Post
       user_id = tuple['user_id']
       username = tuple['username']
       timestamp = tuple['post_date']
+      updated_on = tuple['last_updated_date']
       content = tuple['content']
-      Post.new(id, title, user_id, username, timestamp, content)
+      Post.new(id, title, user_id, username, timestamp, updated_on, content)
     end
   end
 
